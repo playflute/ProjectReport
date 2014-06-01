533fac821d6f8152d0875e5a953dde3652e90353
playflute <playflute@icloud.com>
2014-06-01 16:57:51 +1000

    THis is the week 12 code

--- ExtractDataFromExcel/Extraction/Program.cs
+++ ExtractDataFromExcel/Extraction/Program.cs
@@ -5,31 +5,53 @@ using System.Text;
 using System.Data.OleDb;
 using System.Data;
 using Common;
-
+using System.Threading;
+using System.IO;
 namespace Extraction
 {
-    /// <summary>
-    /// 还需要添加每隔一小时进行数据刷新功能
-    /// </summary>
+
     class Program
     {
         private DataSet ds = null;
         private DateTime today = DateTime.Today;//get today's datetime
         private Dictionary<string, Int32> Activity_Dic = new Dictionary<string, int>();
-       
+        //*******************
+        public void StartTimer(int dueTime)
+        {
+            Timer t = new Timer(new TimerCallback(TimerProc));
+            t.Change(0, dueTime);//the first parameter is the delay,second is the timer interval
+           
+        }
+        private void TimerProc(object state)
+        {
+            // The state object is the Timer object.
+
+            //Timer t = (Timer)state;
+            //t.Dispose();
+            this.Clean_DB_Table();//clean the stale data
+            Activity_Dic.Clear();//clear the classification dictionary
+            this.ds=this.getDataSet();//read the excel and get new data set
+            this.GenerateXML();
+            this.classify();//classify the activity update the dictionary and write into the database again
+            this.Show_ActivityNumber();//show activity number by category
+            this.Show_TotalActivityNumber();//show total activity number
+            Console.WriteLine("Last week event number is :" + this.Last_Week_Event_Number());
+            Console.WriteLine("Last two week event number is :" + this.Last_TwoWeek_Event_Number());
+            Console.WriteLine("Last month event number is: " + this.Last_Month_Event_Number());
+            Console.WriteLine("Update once at "+DateTime.Now.ToString());
+            Console.WriteLine("*************************The timer callback executes.*******************");
+        }
+        //*************************
         static void Main(string[] args)
         {
             Program p = new Program();
-            p.ds = p.getDataSet();
-            p.classify();
-            p.Show_ActivityNumber();
-            p.Show_TotalActivityNumber();
-            Console.WriteLine("Last week event number is :"+p.Last_Week_Event_Number());
-            Console.WriteLine("Last two week event number is :" + p.Last_TwoWeek_Event_Number());
-            Console.WriteLine("Last month event number is: "+p.Last_Month_Event_Number());
-
-            Console.ReadKey();
+            p.StartTimer(30000);//update the database every half an hour,that is 1800s=1800,000ms
+            
+            Console.ReadLine();
         }
+        /// <summary>
+        /// classify the activity update the dictionary and write into the database again
+        /// </summary>
         public void classify()
         {
             
@@ -83,6 +105,9 @@ namespace Extraction
             }
  
         }
+        /// <summary>
+        /// show activity number by category
+        /// </summary>
         public void Show_ActivityNumber()
         {
             
@@ -92,15 +117,23 @@ namespace Extraction
             }
  
         }
+        /// <summary>
+        /// show total activity number
+        /// </summary>
         public void Show_TotalActivityNumber()
         {
-            int sum = 0;
-            foreach (int a in Activity_Dic.Values)
-            {
-                sum += a;
+            //int sum = 0;
+            //foreach (int a in Activity_Dic.Values)
+            //{
+            //    sum += a;
 
+            //}
+            using (Week8Container container = new Week8Container())
+            {
+                Console.WriteLine("Total event Number is" + container.DurationSet.Count());
+                
             }
-            Console.WriteLine("Total event Number is" + sum);
+          
  
         }
         public int Last_Week_Event_Number()
@@ -159,12 +192,18 @@ namespace Extraction
 
 
         }
+        /// <summary>
+        /// read the excel and get new data set
+        /// </summary>
+        /// <returns></returns>
         private DataSet getDataSet()
         {
-            string FilePath = @"c:\users\zhigang zhang\documents\visual studio 2010\Projects\ExtractDataFromExcel\Extraction\duration.xlsx";
+
+            string relative_Path = @"..\..\duration.xlsx";
+            //string absolute_FilePath = @"c:\users\zhigang zhang\documents\visual studio 2010\Projects\ExtractDataFromExcel\Extraction\duration.xlsx";
             string strConn;
 
-            strConn="Provider=Microsoft.ACE.OLEDB.12.0;" +"Data source=" + FilePath + ";" +"Extended Properties=Excel 8.0";
+            strConn = "Provider=Microsoft.ACE.OLEDB.12.0;" + "Data source=" + relative_Path + ";" + "Extended Properties=Excel 8.0";
 
             OleDbConnection conn = new OleDbConnection(strConn);
 
@@ -189,6 +228,37 @@ namespace Extraction
             return myDataSet;
  
         }
-        //According to the data in duration table,the base date today is 
+
+        private void Clean_DB_Table()
+        {
+            
+            using (Week8Container container = new Week8Container())
+            {
+                var result=from needTODelete in container.DurationSet select needTODelete;
+                if (result.Count() > 0)
+                {
+                    foreach (var item in result)
+                    {
+                        container.DurationSet.DeleteObject(item);
+                    }
+                }
+                container.SaveChanges();
+               
+            }
+            
+            
+
+        }
+        private void GenerateXML()
+        {
+            
+            string xmlDoc = this.ds.GetXml();
+            StreamWriter sw = new StreamWriter("duration.xml",false);
+            sw.Write(xmlDoc);
+            sw.Flush();
+            sw.Close();
+        }
+
+        
     }
 }
